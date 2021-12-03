88de8d1a62d5717e6b43760ec286ea0c4d921dd9
begeri <91968768+begeri@users.noreply.github.com>
2021-12-03 23:13:47 +0100

    Megkommentáltam a myMCTS.py -t
    Részletesebb függvényleírások, pontosított kommentelések, és mivel végül nem használtuk az értékelőfüggvényben a mobility-t, ezeket kitöröltem.
    
    Azért nem használtuk a mobilityt, mert nem triviális, hogy nézem a világos és a sötét nézőpontjából.

--- myMCTS.py
+++ myMCTS.py
@@ -11,6 +11,9 @@ vboard = chess.Board()
 play_as=None
 
 class Node():
+  '''
+  Csúcsosztály, amikkel lehet fát építeni. Tartalmazza a játékállást FEN kódolásban és a kód futása során használt jellemzőit az állásnak
+  '''
   def __init__(self, state):
     #milyen játékállás ez, fen stringként
     self.state=state
@@ -18,18 +21,16 @@ class Node():
     self.children=[]
     #még nem nézett lépések
     vboard.set_fen(state)
-    self.not_visited=list(vboard.legal_moves)
-    #mobility
-    self.mobility=len(list(vboard.legal_moves))
+    self.not_visited=list(vboard.legal_moves)   
     #szülő a fában
     self.parent=None
-    #number of simulations
+    #a leszármazottaiból lefuttatott kiértékelések száma
     self.N=0
-    #number of wins
+    ##a leszármazottak kiértékeléseinek összértéke
     self.w=0
     #a lépés, amivel ideérkeztünk
     self.action = ''
-    #vége van e
+    #ezen állásban vége van-e a játéknak
     self.outcome = None
   
   def __str__(self):
@@ -42,7 +43,11 @@ def ucb(node):
   result = node.w+c*math.sqrt((math.log(node.parent.N)+10**-6)/(node.N+10**-10))
   return result
 
+
 def pieces(fen):
+  '''
+  Egy fen kódolású állásból kigyűjti, az egyes figuratípusokból hány darab van a pályán.
+  '''
   pieces = {}
   for char in 'KkQqRrNnBbPp':
     pieces[char] = 0
@@ -53,7 +58,7 @@ def pieces(fen):
   
 def eval(node):
   '''
-  Világosként nézve összeadjuk a pályán lévő figurák értékét hogy sakk van-e, illetve hogy nyertünk-e
+  Világosként megnézzük, hogy nyertünk-e, ha nem összeadjuk a pályán lévő figurák értékét. Így értékeljük az állást.
   '''
   if node.outcome != None:
     return node.outcome
@@ -64,28 +69,10 @@ def eval(node):
   knightWt=3
   bishopWt=3
   pawnWt=1
-  mobilityWt=0.1
   # score max = 39 egyik készletre nézve
-  score=queenWt*(p['Q']-p['q']) + rookWt*(p['R']-p['r']) + knightWt*(p['N']-p['n']) + bishopWt*(p['B']-p['b']) + pawnWt*(p['P']-p['p'])# + mobilityWt*node.mobility
+  score=queenWt*(p['Q']-p['q']) + rookWt*(p['R']-p['r']) + knightWt*(p['N']-p['n']) + bishopWt*(p['B']-p['b']) + pawnWt*(p['P']-p['p'])
   #return score/30
   return math.tanh(score)/(score+1.01)
-
-def rollout(node):
-  '''
-  Plays out a random game, on the given node's board
-  '''
-  vboard.set_fen(node.state)
-
-  while(vboard.outcome()==None):
-    legal_moves=list(vboard.legal_moves)
-    vboard.push(random.choice(legal_moves))
-  
-  if vboard.result()=='1/2-1/2':
-    return 0.5
-  winner = (vboard.result()=='1-0')
-  if winner==play_as:
-    return 1
-  return 0
   
 def selection(node):
   
@@ -94,6 +81,7 @@ def selection(node):
   Ha van olyan gyereke az adott csúcsnak, amit még nem vizsgáltunk, visszaadjuk ezt a csúcsot.
   Ha nincs ilyen gyereke a vizsgált csúcsnak, vasszük a gyerekek közül a maxUCB értékűt. És erre a selection() 
   '''
+  #Ha van a csúcsnak mg nem vizsgált lépése
   if node.not_visited:
     return node
   #A gyerekekből a max UCB értékűt kiválasztani
@@ -101,9 +89,14 @@ def selection(node):
     maxUCB_child=max(node.children, key=lambda i: ucb(i))
     #és meghívni rá a selection-t
     return selection(maxUCB_child)
+  #Ha vége van a játéknak, nem tudunk innen tovább lépni.
   return node
 
 def expansion(node):
+  '''
+  Egy adott levélnek egy lehetséges lépését hozzá akarjuk adni a fához. Létrehozunk egy 'child' csúcsot az input még meg nem nézett lépéseiből az egyiket meglépve.
+  Ennek a megfelelő adattagjait beállítjuk, illetve a szülő még nem látott gyerekeiből kitöröljük és hozzáadjuk a már látott gyerekekhez.
+  '''
   #move-ot kiválasztani, és törölni a moveot a parent.not_visited listából
   move=random.choice(node.not_visited)
   node.not_visited.remove(move)
@@ -120,8 +113,10 @@ def expansion(node):
     # FEHÉR NYER
     if chess.Board.outcome(vboard).winner:
       child.outcome = 1
+    # FEKETE NYER
     elif not chess.Board.outcome(vboard).winner:
       child.outcome = -1
+    #DÖNTETLEN
     else:
       child.outcome = 0.5
 
@@ -130,27 +125,44 @@ def expansion(node):
   return child
 
 def rollback(node, result):
+   '''
+  Egy új kiértékeléssel frissítjük a fában az input csúcsot és az őseit.
+  '''
   node.w += result
   node.N += 1
   if node.parent!=None:
     rollback(node.parent, result)
 
 def make_move(board, time_limit):
+  '''
+  board - játékállás, chess.Board típusú objektum
+  time_limit - gondolkodási idő másodpercben
+  Megkap egy játékállást, eldönti milyen játékos lép innen, majd minden értékelést eszerint végezve a kapott gondolkodási idő hosszáig építi a keresőfát,
+  finomítja a csúcsok értékelését.
+  Majd visszaadja a fában az állás legígéretesebbnek tűnő gyerekének a lépését. 
+  '''
+  #Gyökér inicializálása
   root=Node(board.fen())
+  #Milyen játékosként játszunk
   player = board.fen().split()[1]
   if player == 'w':
     play_as=1
   else:
+    #Mivel az értékelőfüggvény a fehér szempontjából nézi az ábrát -1-szeresét kell venni az értéknek
     play_as=-1
-  print(play_as)
-  #amíg van idő, addig selection, expansion, rollout, rollback, repeat
+  #print(play_as) - for testing sake
+  
+  #amíg van idő, addig selection, expansion, eval, rollback, repeat
   begin = datetime.datetime.utcnow()
   while datetime.datetime.utcnow() - begin < datetime.timedelta(seconds=time_limit):
+    #A selection megad egy olyan leszármazottat, aminek van még nem vizsgált lépése
     child=selection(root)
     if child.outcome==None:
+      #Létrehoz egy eddig nem vizsgált játékállást
       new_child = expansion(child)
+    #Frissíti az új gyerek őseinek az értékelését az új gyerek értékétől függően
     rollback(new_child, play_as*eval(new_child))
-  print('#simulations = ', root.N)
+  #print('#simulations = ', root.N) - testing stuff
   best_child = max(root.children, key=lambda i: i.w/i.N)
-  print('best_eval', play_as*eval(best_child))
+  #print('best_eval', play_as*eval(best_child)) - testing stuff
   return best_child.action
